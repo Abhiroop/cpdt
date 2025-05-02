79eee60772885af997020a71ac0fb03b643d1c5b
Abhiroop <asiamgenius@gmail.com>
2025-05-02 20:16:44 +0200

    Proof till now

--- src/StackMachineAttempt.v
+++ src/StackMachineAttempt.v
@@ -1,3 +1,43 @@
 Require Import Bool Arith List Cpdt.CpdtTactics.
 Set Implicit Arguments.
 Set Asymmetric Patterns.
+
+Inductive binop : Set := Plus | Times.
+
+Inductive exp : Set :=
+| Const : nat -> exp
+| Binop : binop -> exp -> exp -> exp.
+
+Definition binopDenote (b : binop) : nat -> nat -> nat :=
+  match b with
+  | Plus  => plus
+  | Times => mult
+  end.
+
+
+Fixpoint expDenote (e : exp) : nat :=
+  match e with
+  | Const n => n
+  | Binop b e1 e2 => (binopDenote b) (expDenote e1) (expDenote e2)
+  end.
+
+Eval simpl in expDenote (Const 42).
+Eval simpl in expDenote (Binop Plus (Const 2) (Const 2)).
+Eval simpl in expDenote (Binop Times (Binop Plus (Const 2) (Const 2)) (Const 7)).
+
+Inductive instr : Set :=
+| iConst : nat -> instr
+| iBinop : binop -> instr.
+
+Definition prog := list instr.
+Definition stack := list nat.
+
+Definition instrDenote (i : instr) (s : stack) : option stack :=
+  match i with
+  | Const n => Some (n :: s)
+  | iBinop b =>
+      match s with
+      | arg1 :: arg2 :: s' => Some ((binopDenote b) arg1 arg2 :: s')
+      | _ => None.
+      end.
+  end.
