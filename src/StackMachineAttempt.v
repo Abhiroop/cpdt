ce561f62797d9651836e1237708f1c898e1a3081
Abhiroop <asiamgenius@gmail.com>
2025-05-03 14:47:16 +0200

    Proof1

--- src/StackMachineAttempt.v
+++ src/StackMachineAttempt.v
@@ -34,10 +34,71 @@ Definition stack := list nat.
 
 Definition instrDenote (i : instr) (s : stack) : option stack :=
   match i with
-  | Const n => Some (n :: s)
+  | iConst n => Some (n :: s)
   | iBinop b =>
       match s with
       | arg1 :: arg2 :: s' => Some ((binopDenote b) arg1 arg2 :: s')
-      | _ => None.
-      end.
+      | _ => None
+      end
   end.
+
+Fixpoint progDenote (p : prog) (s : stack) : option stack :=
+  match p with
+  | nil => Some s
+  | i :: p' =>
+      match instrDenote i s with
+      | None => None
+      | Some s' => progDenote p' s'
+      end
+  end.
+
+Fixpoint compile (e : exp) : prog :=
+  match e with
+  | Const n => iConst n :: nil
+  | Binop b e1 e2 => compile e2 ++ compile e1 ++ iBinop b :: nil
+  end.
+
+Eval simpl in compile (Const 42).
+Eval simpl in compile (Binop Plus (Const 2) (Const 2)).
+Eval simpl in compile (Binop Times (Binop Plus (Const 2) (Const 2)) (Const 7)).
+Eval simpl in progDenote (compile (Const 42)) nil.
+Eval simpl in progDenote (compile (Binop Plus (Const 2) (Const 2))) nil.
+
+Lemma compile_correct' : forall e p s,
+    progDenote (compile e ++ p) s = progDenote p (expDenote e :: s).
+  induction e.
+  intros.
+  unfold compile.
+  unfold expDenote.
+  unfold progDenote at 1.
+  simpl.
+  fold progDenote.
+  reflexivity.
+  intros.
+  unfold compile.
+  fold compile.
+  unfold expDenote.
+  fold expDenote.
+  rewrite <- app_assoc.
+  rewrite IHe2.
+  rewrite <- app_assoc.
+  rewrite IHe1.
+  unfold progDenote at 1.
+  simpl.
+  fold progDenote.
+  reflexivity.
+Qed.
+
+Lemma compile_correct'' : forall e p s,
+    progDenote (compile e ++ p) s = progDenote p (expDenote e :: s).
+  induction e; crush.
+Qed.
+
+Theorem compile_correct :
+  forall e, progDenote (compile e) nil = Some (expDenote e :: nil).
+  intros.
+  rewrite <- (app_nil_r (compile e)).
+  rewrite compile_correct''.
+  reflexivity.
+  Qed.
+
