b4e7792ee5f9162f1ce0be36e97729da2ff8a351
Abhiroop <asiamgenius@gmail.com>
2025-05-04 20:29:50 +0200

    Proof

--- src/StackMachineAttempt.v
+++ src/StackMachineAttempt.v
@@ -102,3 +102,45 @@ Theorem compile_correct :
   reflexivity.
   Qed.
 
+
+
+Inductive type : Set := Nat | Bool.
+
+Inductive tbinop : type -> type -> type -> Set :=
+| TPlus  : tbinop Nat Nat Nat
+| TTimes : tbinop Nat Nat Nat
+| TEq    : forall t, tbinop t t Bool
+| TLt    : tbinop Nat Nat Bool.
+
+
+Inductive texp : type -> Set :=
+| TNConst : nat -> texp Nat
+| TBConst : bool -> texp Bool
+| TBinop : forall t1 t2 t, tbinop t1 t2 t -> texp t1 -> texp t2 -> texp t.
+
+Definition typeDenote (t : type) : Set :=
+  match t with
+  | Nat => nat
+  | Bool => bool
+  end.
+
+Definition tbinopDenote arg1 arg2 res (b : tbinop arg1 arg2 res)
+  : typeDenote arg1 -> typeDenote arg2 -> typeDenote res :=
+  match b with
+  | TPlus => plus
+  | TTimes => mult
+  | TEq Nat => beq_nat
+  | TEq Bool => eqb
+  | TLt => leb
+  end.
+
+Fixpoint texpDenote t (e : texp t) : typeDenote t :=
+  match e with
+  | TNConst n => n
+  | TBConst b => b
+  | TBinop _ _ _ b e1 e2 => (tbinopDenote b) (texpDenote e1) (texpDenote e2)
+  end.
+
+Eval simpl in texpDenote (TBinop TTimes (TBinop TPlus (TNConst 2) (TNConst 2))
+                            (TNConst 7)).
+
