b889210c80e5fe5a9b565c808f86e45fa2656612
Abhiroop <asiamgenius@gmail.com>
2025-05-13 17:25:11 +0200

    Add a container typeclass

--- src/Proving.v
+++ src/Proving.v
@@ -47,6 +47,48 @@ Definition to_One (A : Type) (ext : Ext_One A) : One A := tt.
 Definition from_One (A : Type) (z : One A) : Ext_One A :=
   ext tt (fun (p : Pos_One _) => match p with end).
 
-Lemma to_from__One : forall A (ox : One A), to_One (from_One ox) = ox.
+Lemma to_from_One : forall A (ox : One A), to_One (from_One ox) = ox.
   crush.
 Qed.
+
+Lemma from_to_One : forall A (e : Ext_One A), from_One (to_One e) = e.
+  induction e; crush.
+  unfold to_One.
+  unfold from_One.
+  (* f_equal : ∀ (A B : Type) (f : A → B) (x y : A), x = y → f x = f y *)
+  apply f_equal.
+  (* Extensionality:
+     (∀x, f x = g x) -> f = g.
+
+     Tactic wise, we have here
+     λ p => body  =  a
+
+     we have f = g; we can apply p to both side
+     and introduce p into the goal;
+
+    body = a p
+    p applied to both side
+   *)
+  extensionality p.
+  destruct p.
+Qed.
+
+Class Container F :=
+  {
+    Shape : Type;
+    Pos   : Shape -> Type;
+    to    : forall A, Ext Shape Pos A -> F A;
+    from  : forall A, F A -> Ext Shape Pos A;
+    to_from : forall A (fx : F A), to (from fx) = fx;
+    from_to : forall A (e : Ext Shape Pos A), from (to e) = e
+  }.
+
+Instance C_One : Container One :=
+  {
+    Shape := Shape_One;
+    Pos   := Pos_One;
+    to    := to_One;
+    from  := from_One;
+    to_from := to_from_One;
+    from_to := from_to_One
+  }
