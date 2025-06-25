dc3cf9e33eeba76766fbfee9ea5ab11414a80994
Abhiroop <asiamgenius@gmail.com>
2025-06-25 13:03:19 +0200

    Add Free

---
+++ src/Free.v
@@ -0,0 +1,7 @@
+From Coq Require Import String Nat.
+From Coq Require Import ZArith Ascii List RelationClasses Eqdep.
+Require Import Monads.
+Import Monad.
+Import ListNotations.
+Import MonadLawsEqM.
+Import MonadNotation.
